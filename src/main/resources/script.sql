0fa8fd8d5594138f25fa5a3b8c347deee453d8ee
Juan Guillermo Gomez Torres <juan.gomez01@gmail.com>
2019-07-29 21:35:14 -0500

    se agrega script sql

---
+++ src/main/resources/script.sql
@@ -0,0 +1,9 @@
+Insert into accountdb.account(blood_type, date_last_query) values ('A+', '12/05/19');
+
+Insert into accountdb.account(blood_type, date_last_query) values ('B+', '12/05/19');
+
+Insert into accountdb.allergie(date, medicine, name, account_id) values ('12/05/19', 'ospirin 100mg', 'Rinitivis', 1);
+
+Insert into accountdb.allergie( date, medicine, name, account_id) values ('12/05/20', 'Aspirina', 'Otra Alergia', 1);
+
+Insert into accountdb.allergie(date, medicine, name, account_id) values ('12/05/19', 'ospirin 100mg', 'Rinitivis', 2)
